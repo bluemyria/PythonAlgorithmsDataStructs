540d009e6fe74b6ba213a6a54d37732620d063bd
bluemyria <bluemyria@users.noreply.github.com>
2020-02-04 00:40:12 +0100

    13. Sorting (part b)

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -1,10 +1,19 @@
 from test_framework import generic_test
+from sorted_lists_merge import merge_two_sorted_lists
 
-
+# 13.10 Implement a fast sorting algorithm for lists
+# Implement a routine which sorts lists efficiently. It should be a stable sort, i.e., the relative positions
+# of equal elements must remain unchanged.
 def stable_sort_list(L):
-    # TODO - you fill in here.
-    return None
-
+    if not L or not L.next:
+        return L
+    prev_slow, slow, fast = None, L, L
+    while fast and fast.next:
+        prev_slow = slow
+        slow = slow.next
+        fast = fast.next.next
+    prev_slow.next = None
+    return merge_two_sorted_lists(stable_sort_list(L), stable_sort_list(slow))
 
 if __name__ == '__main__':
     exit(
