17c29b654af83f70a6a261813c4af8ac89955eb5
bluemyria <bluemyria@users.noreply.github.com>
2020-05-07 03:44:30 +0200

    Graphs BFS, DFS, Dijkstra...

--- bluemyria_notes/_Graphs.py
+++ bluemyria_notes/_Graphs.py
@@ -4,12 +4,18 @@ class Vertex:
         self.connectedTo = {}
         self.distance = -1    # for BFS
         self.predecessor = -1 # for BFS
-        self.color = "white"  # for BFS
-        self.discoveryTime = -1   # for DFS
-        self.finishTime = -1      # for DFS
+        self.color = "white"  # for BFS & DFS
+        self.discoveryTime = -1     # for DFS
+        self.finishTime = -1        # for DFS
 
     def addNeighbor(self,nbr,weight=0):
         self.connectedTo[nbr]=weight
+
+    def setDistance(self, dist=0):
+        self.distance = dist
+    
+    def getDistance(self):
+        return self.distance
 
     def __str__(self):
         return (str(self.id) + ' \ndistance: ' + str(self.distance)
@@ -19,6 +25,9 @@ class Vertex:
                + " \nfinish " + str(self.finishTime)
                #+ ' \nconnectedTo: ' + str([x.id for x in self.connectedTo])
                )
+    
+    def __lt__(self, other):
+        return self.distance < other.getDistance()
 
     def getConnections(self):
         return self.connectedTo.keys()
@@ -242,5 +251,281 @@ g.addEdge(3, 3)
 print ("Following is Breadth First Traversal"
 				" (starting from vertex 2)") 
 g.BFS(2) 
+print('\n\n')
 
 # This code is contributed by Neelam Yadav 
+
+
+
+# from Grokking Algorithms & MIT
+import collections
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anuj", "peggy", 'claire']
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anuj"] = []
+graph["peggy"] = ['bob']
+graph["thom"] = []
+graph["jonny"] = []
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+def bfs_name(s):
+    search_queue = collections.deque()
+    search_queue += graph[s]
+    # print(search_queue)
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            print('checking: ', person)
+            if person_is_seller(person):
+                print('found!', person)
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+bfs_name('you')
+bfs_name('peggy')
+
+print('bfs_name_mit')
+# O(V+E)
+def bfs_name_mit(start, Adj):
+    visited = {start: 0}
+    parent = {start: None}
+    frontier_nr = 1
+    frontier = [start]
+    while frontier:
+        next = []
+        for u in frontier:
+            for v in Adj[u]:
+                if not v in visited:
+                    print('checking: ', v)
+                    visited[v] = frontier_nr
+                    parent[v] = u
+                    next.append(v)
+        frontier = next
+        frontier_nr += 1
+    print(visited)
+    print(parent)
+
+bfs_name_mit('you', graph)
+bfs_name_mit('peggy', graph)
+
+
+# from MIT & me (mix all interesting aspects from all books)
+print ("\n\n***** from MIT & me (mix all interesting aspects from all books) ******") 
+
+def dfs_mit(V, Adj):
+
+    def dfs_visit_mit(s):
+        
+        nonlocal mytime
+        discovery_time[s] = mytime
+        color[s] = "grey"
+        # process_vertex() if desired
+        print('processing.. ', s, 'time: ', mytime)
+        mytime += 1
+        for v in Adj[s]:  # start v, process_edge() if desired
+            print('v, process_edge() ', v)
+            if v not in parent:
+                parent[v] = s
+                dfs_visit_mit(v)
+                # finish v
+            else: # v in parent
+                if color[v] == 'grey':
+                    print("-----------------> backward Edge: CYCLE!!!", s, v)
+                if color[v] == 'black':
+                    if discovery_time[s] < discovery_time[v]:
+                        print("-----------------> forward edge: ", s, v)
+                    else:
+                        print("-----------------> cross edge: ", s, v)
+        finish_time[s] = mytime
+        color[s] = "black"
+        mytime += 1
+        
+    mytime = 0
+    parent = {} 
+    discovery_time = {}
+    finish_time = {}
+    color = {} # make all vertices "white"
+    for v in V:
+        color[v] = "white"
+    
+    for s in V:
+        if s not in parent: # or check if still white
+            parent[s] = None
+            dfs_visit_mit(s)
+
+    print('\n\nparent: ', parent)
+    print('discovery_time: ', discovery_time)
+    print('finish_time: ', finish_time)
+    print('color: ', color)
+    print('JOB SCHEDULING: ')
+    for w in sorted(finish_time, key=finish_time.get, reverse=True):
+        print(w, finish_time[w])
+    
+
+graph["Mary"] = ["Peter", "Helen"]
+graph["Helen"] = ["alice"]
+graph["Peter"] = []
+
+print(graph.keys())
+print(graph)
+dfs_mit(graph.keys(), graph)
+
+# Job scheduling
+# Given Directed Acylic Graph (DAG), where vertices represent tasks & edges represent
+# dependencies, order tasks without violating dependencies
+# run DFS and order in the reverse finish_time
+# better to start from vertices w/o incoming edges (not implemented here)
+jobsched = {}
+jobsched["A"] = ["B", "H"]
+jobsched["B"] = ["C"]
+jobsched["C"] = ["F"]
+jobsched["D"] = ["B", "E"]
+jobsched["E"] = ["F"]
+jobsched["F"] = []
+jobsched["G"] = ["H"]
+jobsched["H"] = []
+jobsched["I"] = []
+print(jobsched.keys())
+print(jobsched)
+dfs_mit(jobsched.keys(), jobsched)
+
+
+# Iterative Depth First Search of a Graph // from DSA in Python
+def graphDFS(V, Adj, start, goal):
+    stack = []
+    visited = set()
+    stack.append(start)
+    
+    while stack:
+        current = stack.pop()
+        visited.add(current)
+    
+        if current == goal:
+            print(visited)
+            return True # or return path to goal perhaps
+    
+        for v in Adj[current]:
+            if not v in visited:
+                stack.append(v)
+    
+    return False # or return an empty path
+graphDFS(graph.keys(), graph, "Mary", "claire")
+
+import heapq
+def dijkstra(aGraph,start):
+    pq = []
+    start.setDistance(0)
+    pq = heapq.heapify([(v.getDistance(),v) for v in aGraph])
+    print(pq)
+    while pq:
+        currentVert = heapq.heappop(pq)
+        for nextVert in currentVert.getConnections():
+            newDist = currentVert.getDistance() \
+                    + currentVert.getWeight(nextVert)
+            if newDist < nextVert.getDistance():
+                nextVert.setDistance( newDist )
+                nextVert.setPred( currentVert )
+                pq.decreaseKey( nextVert, newDist )
+
+
+g3 = Graph()
+g3.addVertex('u')
+g3.addVertex('v')
+g3.addVertex('w')
+g3.addVertex('x')
+g3.addVertex('y')
+g3.addVertex('z')
+g3.addEdge('u','v',2)
+g3.addEdge('u','w',5)
+g3.addEdge('u','x',1)
+g3.addEdge('v','w',3)
+g3.addEdge('v','x',2)
+g3.addEdge('x','w',3)
+g3.addEdge('x','y',1)
+g3.addEdge('w','y',1)
+g3.addEdge('w','z',5)
+g3.addEdge('y','z',1)
+g3.addEdge('v','u',2)
+g3.addEdge('w','u',5)
+g3.addEdge('x','u',1)
+g3.addEdge('w','v',3)
+g3.addEdge('x','v',2)
+g3.addEdge('w','x',3)
+g3.addEdge('y','x',1)
+g3.addEdge('y','w',1)
+g3.addEdge('z','w',5)
+g3.addEdge('z','y',1)
+for v in g3:
+    print(v)
+    for w in v.getConnections():
+        print("( %s -> %s, %d )" % (v.getId(), w.getId(), v.getWeight(w)))  
+print(g3.getVertices())
+print('*************** AFTER DIJKSTRA ***************')
+dijkstra(g3, g3.getVertex('u'))
+for v in g3:
+    print(v)
+    for w in v.getConnections():
+        print("( %s -> %s, %d )" % (v.getId(), w.getId(), v.getWeight(w)))  
+
+
+from collections import namedtuple, deque
+from pprint import pprint as pp
+ 
+inf = float('inf')
+Edge = namedtuple('Edge', ['start', 'end', 'cost'])
+
+class DijkstraGraph():
+    def __init__(self, edges):
+        self.edges = [Edge(*edge) for edge in edges]
+        # pp(self.edges[0])
+        self.vertices = {e.start for e in self.edges} | {e.end for e in self.edges}
+        # pp(self.vertices)
+
+    def dijkstra(self, source, dest):
+        assert source in self.vertices
+        
+        dist = {vertex: inf for vertex in self.vertices}
+        dist[source] = 0
+        # pp(dist)
+
+        previous = {vertex: None for vertex in self.vertices}
+        # pp(previous)
+        
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for start, end, cost in self.edges:
+            neighbours[start].add((end, cost))
+        # pp(neighbours)
+ 
+        q = self.vertices.copy()
+        
+        while q:
+            # pp(q)
+            u = min(q, key=lambda vertex: dist[vertex])
+            q.remove(u)
+            if dist[u] == inf or u == dest:
+                break
+            for v, cost in neighbours[u]:
+                alternative = dist[u] + cost
+                if alternative < dist[v]:       # Relax (u,v,a)
+                    dist[v] = alternative
+                    previous[v] = u
+        # pp(previous)
+        sp = deque()
+        u = dest
+        while previous[u]:
+            sp.appendleft(u)
+            u = previous[u]
+        sp.appendleft(u)
+        return sp
+
+
+mygraph = DijkstraGraph([("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
+               ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
+               ("e", "f", 9)])
+pp(mygraph.dijkstra("a", "e"))
